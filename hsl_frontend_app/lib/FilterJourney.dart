a85ecacc1789bd80c0144bee43491fc3c30c9817
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2023-05-21 16:13:37 +0300

    added filterjourney model and api ui

---
+++ hsl_frontend_app/lib/FilterJourney.dart
@@ -0,0 +1,28 @@
+class FilterJourney {
+  FilterJourney({
+    required this.totalReturn,
+    required this.totalDeparture,
+    required this.avgReturn,
+    required this.avgDeparture,
+  });
+  late final int totalReturn;
+  late final int totalDeparture;
+  late final String avgReturn;
+  late final String avgDeparture;
+
+  FilterJourney.fromJson(Map<String, dynamic> json) {
+    totalReturn = json['totalReturn'];
+    totalDeparture = json['totalDeparture'];
+    avgReturn = json['avgReturn'];
+    avgDeparture = json['avgDeparture'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['totalReturn'] = totalReturn;
+    _data['totalDeparture'] = totalDeparture;
+    _data['avgReturn'] = avgReturn;
+    _data['avgDeparture'] = avgDeparture;
+    return _data;
+  }
+}
